e659f51c73cd72d2f355ab180bc653d29cf45b29
Chandan <chandanthakur.ct@gmail.com>
2023-04-12 14:43:22 +0530

    commented

--- src/main/java/com/example/myproject/config/SwaggerConfig.java
+++ src/main/java/com/example/myproject/config/SwaggerConfig.java
@@ -1,14 +1,14 @@
-package com.example.myproject.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class SwaggerConfig {
-
-	public OpenApi swaggerConfiguration() {
-		return new OpenApi()
-				.components(new Components())
-				.info(new Info().title("Spring Boot Demo Project")
-						.description("Testing the OpenAPI"));
-	}
-}
+//package com.example.myproject.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class SwaggerConfig {
+//
+//	public OpenApi swaggerConfiguration() {
+//		return new OpenApi()
+//				.components(new Components())
+//				.info(new Info().title("Spring Boot Demo Project")
+//						.description("Testing the OpenAPI"));
+//	}
+//}
